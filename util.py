4c376efdb4658812fcb7a1b06bb21ef8d3666bf1
Sarah Etter <sarahett@usc.edu>
2020-09-07 21:00:39 -0400

    just fix the coefficient brackets

--- util.py
+++ util.py
@@ -43,18 +43,38 @@ def lin_reg_mod(df, x, y):
     y_pred = reg.predict(X_test)
 
     # coefficients
-    print(f"Coefficients: {reg.coef_}")
+    linCoeff = f"Coefficients: {reg.coef_}"
     # mean squared error
-    print('Mean squared error: %.2f' % mean_squared_error(y_test, y_pred))
+    meanSqErr = 'Mean squared error: %.2f' % mean_squared_error(y_test, y_pred)
     # coefficient of determination (1 = perfect prediction)
-    print('Coefficient of determination: %.2f' % r2_score(y_test, y_pred))
+    dCoef = 'Coefficient of determination: %.2f' % r2_score(y_test, y_pred)
 
-   # plot outputs
-    plt.scatter(X_test, y_test, color='black')
-    plt.plot(X_test, y_pred, color='blue', linewidth=2)
-    plt.title(f"{x} vs {y}")
-    plt.xticks()
-    plt.yticks()
+   # # old plotting method plot outputs
+   #  plt.scatter(X_test, y_test, color='black')
+   #  plt.plot(X_test, y_pred, color='blue', linewidth=2)
+   #  plt.title(f"{x} vs {y}")
+   #  plt.xticks()
+   #  plt.yticks()
+   #
+   #  # labeling axes
+   #  units = {'Weight': '[lb]', 'Distance': '[mi]', 'Carb': '[g]', 'Protein': '[g]', 'Fat': '[g]', 'Fiber': '[g]'}
+   #  label1 = x
+   #  label2 = y
+   #  if x in units:
+   #      label1 += (' ' + units[x])
+   #  if y in units:
+   #      label2 += (' ' + units[y])
+   #  plt.ylabel(f"{label2}")
+   #  plt.xlabel(f"{label1}")
+   #  plt.show()
+
+    # plot outputs
+    plt.style.use('seaborn')
+    fig, ax = plt.subplots()
+    ax.scatter(X_test, y_test, color='black')
+    ax.plot(X_test, y_pred, color='blue', linewidth=2)
+    ax.set_title(f"{x} vs {y}")
+    ax.tick_params(axis='both')
 
     # labeling axes
     units = {'Weight': '[lb]', 'Distance': '[mi]', 'Carb': '[g]', 'Protein': '[g]', 'Fat': '[g]', 'Fiber': '[g]'}
@@ -64,10 +84,27 @@ def lin_reg_mod(df, x, y):
         label1 += (' ' + units[x])
     if y in units:
         label2 += (' ' + units[y])
-    plt.ylabel(f"{label2}")
-    plt.xlabel(f"{label1}")
-    plt.show()
+    ax.set_ylabel(f"{label2}")
+    ax.set_xlabel(f"{label1}")
 
+    # add data below fig
+    textBox = dict(boxstyle='round, pad=1', facecolor='none', edgecolor='black')
+    textStr = f"{linCoeff}\n{meanSqErr}\n{dCoef}"
+    ax.annotate(textStr, xy=(0.5, 0),
+            # credit: https: // stackoverflow.com / questions / 17086847 / box - around - text - in -matplotlib
+            # Interpret the x as axes coords, and the y as figure coords
+            xycoords=('axes fraction', 'figure fraction'),
+
+            # The distance from the point that the text will be at
+            xytext=(0, 10),
+            # Interpret `xytext` as an offset in points...
+            textcoords='offset points',
+
+            # Any other text parameters we'd like
+            size=10, ha='center', va='bottom', bbox=textBox, weight='bold')
+
+    plt.subplots_adjust(bottom=0.25)
+    plt.show()
 
 def pair_hist_plot(X_train, y_train):
     df = pd.DataFrame(X_train, columns=['Calories', 'Steps'])
